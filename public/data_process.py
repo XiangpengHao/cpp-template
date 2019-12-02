037c5a6b14355902b616106c9ad5a72ede094842
Xiangpeng Hao <xha62@sfu.ca>
2019-12-02 15:51:43 -0800

    try to add benchmark

---
+++ public/data_process.py
@@ -0,0 +1,24 @@
+import json
+
+
+def process():
+    data = {}
+    with open('data.json') as f:
+        data = json.load(f)
+        lines = []
+        with open('bench_data') as bench:
+            lines = bench.readlines()[0]
+            lines = lines.split(' ')
+
+        data['data'].append({
+            'gitTag': lines[0][:5],
+            'buildId': int(lines[1]),
+            'dataPoint': int(lines[2])
+        })
+
+    with open('data.json', 'w') as f:
+        json.dump(data, f)
+
+
+if __name__ == '__main__':
+    process()
